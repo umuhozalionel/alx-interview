16445837f891e5ebeefb523f5d9c367b97f01f8a
umuhozalionel <muhozalionel@gmail.com>
2024-11-12 08:55:30 -0800

    Initial commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,35 @@
 #!/usr/bin/node
 const request = require('request');
+
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 request(apiUrl, function (error, response, body) {
   if (error) {
     console.error('Error:', error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
+    return;
+  }
+  
+  const characters = JSON.parse(body).characters;
+  const promises = characters.map((character) => {
+    return new Promise((resolve, reject) => {
       request(character, function (error, response, body) {
         if (error) {
-          console.error('Error:', error);
+          reject(error);
         } else {
-          console.log(JSON.parse(body).name);
+          resolve(JSON.parse(body).name);
         }
       });
     });
-  }
+  });
+
+  Promise.all(promises)
+    .then((names) => {
+      names.forEach((name) => {
+        console.log(name);
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 });
