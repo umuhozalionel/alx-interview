372c04b51c22997853245274b2197bd022dd002e
umuhozalionel <muhozalionel@gmail.com>
2024-11-28 14:03:48 -0800

    Initial commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
-"""
-Making Change
-"""
+"""Making change O(n)"""
 
 
 def makeChange(coins, total):
-    """
-    Determines the fewest number of coins needed to meet a given amount total.
+    """Clasic Bottom-Up dynamic programming"""
+    temp_value = 0
+    coins.sort(reverse=True)
 
-    Args:
-        coins (list): A list of the values of the coins in your possession.
-        total (int): The total amount to be met.
-
-    Returns:
-        int: Fewest number of coins needed to meet total.
-             If total cannot be met, return -1.
-             If total is 0 or less, return 0.
-    """
-    if total <= 0:
+    if total < 0:
         return 0
 
-    # Initialize a list to hold the minimum coins needed for each amount up to total
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0
+    for coin in coins:
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                min_coins[i] = min(min_coins[i],
-                                   min_coins[i - coin] + 1)
+    return temp_value if total == 0 else -1
 
-    return min_coins[total] if min_coins[total] != float('inf') else -1
+
+if __name__ == '__main__':
+
+    print(makeChange([1, 2, 25], 37))
+
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
