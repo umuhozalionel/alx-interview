a6834bbdd0856bcd10a86e4e0e98994f9ce8623d
umuhozalionel <muhozalionel@gmail.com>
2024-11-28 13:16:56 -0800

    Initial commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,6 +3,7 @@
 Making Change
 """
 
+
 def makeChange(coins, total):
     """
     Determines the fewest number of coins needed to meet a given amount total.
@@ -19,13 +20,17 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize a list to hold the minimum coins needed for each amount up to total
+    # Initialize a list to hold the minimum coins needed for each amount
     min_coins = [float('inf')] * (total + 1)
     min_coins[0] = 0
 
     for i in range(1, total + 1):
         for coin in coins:
             if coin <= i:
-                min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+                min_coins[i] = min(min_coins[i],
+                                   min_coins[i - coin] + 1)
 
-    return min_coins[total] if min_coins[total] != float('inf') else -1
+    if min_coins[total] != float('inf'):
+        return min_coins[total]
+    else:
+        return -1
